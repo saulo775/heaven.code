8a39c16ac3a004c5c29667a42aa3a2548f450ba2
saulo775 <59702227+saulo775@users.noreply.github.com>
2020-09-01 14:41:29 -0300

    commit 3 - Seção depoimentos e seção diferenciais

--- index.php
+++ index.php
@@ -67,6 +67,55 @@
         </div>
       </div>
     </section>
+
+    <section class="depoimento text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Depoimento</h2>
+            <blockquote>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tortor tellus, lobortis eget justo non, lobortis lobortis ex. Quisque at tristique lacus. Nam vestibulum ante non bibendum vulputate. Phasellus at tortor diam. Integer dapibus orci sit amet efficitur scelerisque. Mauris quis pharetra ante. Phasellus vel euismod est, vitae placerat lorem. Mauris dictum vehicula euismod. Proin non orci feugiat, fermentum leo eget, tincidunt ex."</p>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="diferenciais text-center">
+      <h2>Conheça nossa Empresa</h2>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-people-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+              </svg>
+            </h3>
+            <h2>Diferencial</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+          </div>
+
+          <div class="col-md-4">
+            <h3><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-hammer" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M9.812 1.952a.5.5 0 0 1-.312.89c-1.671 0-2.852.596-3.616 1.185L4.857 5.073V6.21a.5.5 0 0 1-.146.354L3.425 7.853a.5.5 0 0 1-.708 0L.146 5.274a.5.5 0 0 1 0-.706l1.286-1.29a.5.5 0 0 1 .354-.146H2.84C4.505 1.228 6.216.862 7.557 1.04a5.009 5.009 0 0 1 2.077.782l.178.129z"/>
+              <path fill-rule="evenodd" d="M6.012 3.5a.5.5 0 0 1 .359.165l9.146 8.646A.5.5 0 0 1 15.5 13L14 14.5a.5.5 0 0 1-.756-.056L4.598 5.297a.5.5 0 0 1 .048-.65l1-1a.5.5 0 0 1 .366-.147z"/>
+              </svg>
+            </h3>
+            <h2>Diferencial</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium.</p>
+          </div>
+
+          <div class="col-md-4">
+            <h3>
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+              </svg>
+            </h3>
+            <h2>Diferencial</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
